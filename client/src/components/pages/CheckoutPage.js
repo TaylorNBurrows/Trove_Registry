8e6d38a3bef06c456e77c9bcbeeaadba5bddefd0
Jiliene Circelli <jilienecircelli@gmail.com>
2020-02-27 21:43:37 -0800

    updated files

---
+++ client/src/components/pages/CheckoutPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckoutPage = () => {
+    return (
+        <div>Hello Checkout Page</div>
+    )
+}
+
+export default CheckoutPage
---
+++ client/src/components/pages/DiscoverPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DiscoverPage = () => {
+    return (
+        <div>Hello Discover Page</div>
+    )
+}
+
+export default DiscoverPage
