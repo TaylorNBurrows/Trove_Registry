16b4a9d9faf53174c7c40ce3b45f19c0289b621f
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-02 20:33:15 -0800

    added initial passport structure

--- models/user-model.js
+++ models/user-model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
+  Schema = mongoose.Schema,
+  bcrypt = require('bcrypt'),
+  SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   username: {
@@ -37,32 +37,32 @@ const UserSchema = new Schema({
   }
 });
 
-UserSchema.pre('save', function(next) {
-    var user = this;
+UserSchema.pre('save', function (next) {
+  var user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
     });
+  });
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 const UserPost = mongoose.model("UserPost", UserSchema);
 module.exports = UserPost;
