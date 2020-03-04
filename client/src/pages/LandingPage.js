e37896990c45918f42f35c178a4e1a5b5f2a94a8
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-03 21:16:28 -0800

    added donut gif

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -4,26 +4,19 @@ import Button from '@material-ui/core/Button'
 
 import animatedLogo from '../images/Logos/LogoAnimation.gif'
 import donutGIF from '../images/Donut-Chest.gif'
-import { makeStyles } from '@material-ui/core';
-
-import { ImageBackground } from 'react-native';
 
 
 const LandingPage = () => {
     return (
-        <ImageBackground style={styles.imgBackground}
-            resizeMode='cover'
-            source={donutGIF}>
-            <div>
-                <img src={animatedLogo} alt="Trove Logo" width="350px" />
-                <br></br>
-                <Link to="/signup"><Button>Sign Up</Button></Link>
-                <Link to="/login"><Button>Login</Button></Link>
-            </div>
-
-        </ImageBackground>
-
+        <div
+        // style={{ backgroundImage: `url(${donutGIF})`, backgroundSize: 'cover', backgroundPosition: "center", height: "100vh" }}
+        >
+            <img src={animatedLogo} alt="Trove Logo" width="350px" />
+            <br></br>
+            <Link to="/signup"><Button>Sign Up</Button></Link>
+            <Link to="/login"><Button>Login</Button></Link>
+        </div>
     )
 }
 
-export default LandingPage
+export default LandingPage;
