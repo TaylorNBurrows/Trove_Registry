6e12c2cc2abe04dd69872dac61ba5203b2d1f333
Jennifer Gamez <jenggamez@gmail.com>
2020-03-03 19:46:44 -0800

    made login page

--- client/src/pages/LogInPage.js
+++ client/src/pages/LogInPage.js
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import CardHeader from '@material-ui/core/CardHeader';
 
-const LoginPage = () => {
-    return (
-        <div>Hello Login Page</div>
-    )
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: 400,
+      margin: `${theme.spacing(0)} auto`
+    },
+    loginBtn: {
+      marginTop: theme.spacing(2),
+      flexGrow: 1
+    },
+    header: {
+      textAlign: 'center',
+      background: '#212121',
+      color: '#fff'
+    },
+    card: {
+      marginTop: theme.spacing(10)
+    }
+
+  }),
+);
+
+const LogInPage = () => {
+  const classes = useStyles();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [helperText, setHelperText] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (username.trim() && password.trim()) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [username, password]);
+
+  const handleLogin = () => {
+    if (username === 'abc@email.com' && password === 'password') {
+      setError(false);
+      setHelperText('Login Successfully');
+    } else {
+      setError(true);
+      setHelperText('Incorrect username or password')
+    }
+  };
+
+  const handleKeyPress = (e:any) => {
+    if (e.keyCode === 13 || e.which === 13) {
+      isButtonDisabled || handleLogin();
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <form className={classes.container} noValidate autoComplete="off">
+        <Card className={classes.card}>
+          <CardHeader className={classes.header} title="Login App" />
+          <CardContent>
+            <div>
+              <TextField
+                error={error}
+                fullWidth
+                id="username"
+                type="email"
+                label="Username"
+                placeholder="Username"
+                margin="normal"
+                onChange={(e)=>setUsername(e.target.value)}
+                onKeyPress={(e)=>handleKeyPress(e)}
+              />
+              <TextField
+                error={error}
+                fullWidth
+                id="password"
+                type="password"
+                label="Password"
+                placeholder="Password"
+                margin="normal"
+                helperText={helperText}
+                onChange={(e)=>setPassword(e.target.value)}
+                onKeyPress={(e)=>handleKeyPress(e)}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              size="large"
+              color="secondary"
+              className={classes.loginBtn}
+              onClick={()=>handleLogin()}
+              disabled={isButtonDisabled}>
+              Login
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+    </React.Fragment>
+  );
 }
 
-export default LoginPage
+export default LogInPage;
+
