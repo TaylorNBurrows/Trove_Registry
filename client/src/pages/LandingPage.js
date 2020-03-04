ed41900b4675e872995488ae62c11d403b87deba
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-03 20:31:12 -0800

    meh

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 
-
 import animatedLogo from '../images/Logos/LogoAnimation.gif'
 import donutGIF from '../images/Donut-Chest.gif'
+import { makeStyles } from '@material-ui/core';
+
+import { ImageBackground } from 'react-native';
 
 
 const LandingPage = () => {
     return (
-        <div>
-            <img src={animatedLogo} alt="Trove Logo" width="350px" />
-            <br></br>
-            <Link to="/signup"><Button>Sign Up</Button></Link>
-            <Link to="/login"><Button>Login</Button></Link>
-        </div>
+        <ImageBackground style={styles.imgBackground}
+            resizeMode='cover'
+            source={donutGIF}>
+            <div>
+                <img src={animatedLogo} alt="Trove Logo" width="350px" />
+                <br></br>
+                <Link to="/signup"><Button>Sign Up</Button></Link>
+                <Link to="/login"><Button>Login</Button></Link>
+            </div>
+
+        </ImageBackground>
+
     )
 }
 
