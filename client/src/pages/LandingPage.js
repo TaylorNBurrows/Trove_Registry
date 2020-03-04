4a16c411a83dd7089be890e405a5e982384466da
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-03 20:02:48 -0800

    updated Landing page with logo and buttons

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,19 +1,21 @@
 import React from 'react';
-// import Main from '../components/main/index';
+import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import animatedLogo from '../images/Logos/LogoAnimation.gif'
 
 const LandingPage = () => {
     return (
 
-                <div>
-                <h1>Welcome to Trove!</h1>
-                <p>This is the start of something beautiful</p>
+        <div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <img src={animatedLogo} alt="Trove Logo" width="350px" />
+                    <br></br>
+                    <Link to="/signup"><Button>Sign Up</Button></Link>
+                    <Link to="/login"><Button>Login</Button></Link>
+                </div>
             </div>
-
-
-
-
-
-
+        </div >
 
 
 
