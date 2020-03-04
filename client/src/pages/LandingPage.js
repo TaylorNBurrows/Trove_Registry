f9b9757d8108cda75e7c54ee5500513f91d4c42d
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-03 20:15:02 -0800

    tacos for landingpage

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+
+
 import animatedLogo from '../images/Logos/LogoAnimation.gif'
+import donutGIF from '../images/Donut-Chest.gif'
 
 const LandingPage = () => {
     return (
-
         <div>
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <img src={animatedLogo} alt="Trove Logo" width="350px" />
-                    <br></br>
-                    <Link to="/signup"><Button>Sign Up</Button></Link>
-                    <Link to="/login"><Button>Login</Button></Link>
-                </div>
-            </div>
-        </div >
-
-
-
+            <img src={animatedLogo} alt="Trove Logo" width="350px" />
+            <br></br>
+            <Link to="/signup"><Button>Sign Up</Button></Link>
+            <Link to="/login"><Button>Login</Button></Link>
+        </div>
     )
 }
 
