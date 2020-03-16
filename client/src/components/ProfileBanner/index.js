9d0d57913c77cb3fb9a79950ea4e2b70f76c72f0
Jiliene Circelli <jilienecircelli@gmail.com>
2020-03-15 17:19:06 -0700

    added a banner graphic

--- client/src/components/ProfileBanner/index.js
+++ client/src/components/ProfileBanner/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import banner from '../../images/Banners/Banner1.png'
 
 const useStyles = makeStyles(theme => ({
     teal: {
@@ -12,13 +13,23 @@ const useStyles = makeStyles(theme => ({
         borderRadius: '10px',
         padding: '10px',
     },
+    bannerImg: {
+        height: "250px",
+        width: "auto",
+        borderRadius: "10px",
+        padding: "10px",
+        backgroundImage: { banner },
+        backgroundRepeat: "repeat-x"
+    }
 }));
 
 const ProfileBanner = () => {
     const classes = useStyles();
     return (
         <Fragment>
-            <Grid className={classes.teal} />
+            <Grid className={classes.teal} >
+                <img src={banner} className={classes.bannerImg} />
+            </Grid>
         </Fragment>
     )
 }
